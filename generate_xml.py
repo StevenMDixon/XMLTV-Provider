58398d1833a76eba0e22028cbbbdb9f227253556
steven <stevendixon1128@gmail.com>
2025-11-17 09:55:03 -0500

    add folder gen

--- generate_xml.py
+++ generate_xml.py
@@ -2,6 +2,14 @@
 from channels.rewind import RewindChannel
 from channels.aftermath import AftermathChannel
 
+import os
+
+folder_path = "./xml_schedules"  # Define the path for the folder to be created
+
+# Use os.makedirs with exist_ok=True
+os.makedirs(folder_path, exist_ok=True)
+
+
 channels = [RewindChannel(), AftermathChannel()]
 
 for channel in channels:
